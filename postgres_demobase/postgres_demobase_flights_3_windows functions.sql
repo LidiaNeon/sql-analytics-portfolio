234b1ad08915aa18e6c829e932c88c2aaa2680c8
LidiaNeon <119794259+LidiaNeon@users.noreply.github.com>
2025-12-07 18:22:22 -0300

    Update postgres_demobase_flights_3_windows functions.sql

--- postgres_demobase/postgres_demobase_flights_3_windows functions.sql
+++ postgres_demobase/postgres_demobase_flights_3_windows functions.sql
@@ -1,3 +1,4 @@
+-- Количество рейсов по маршруту на каждую дату
 SELECT
     route_no,
     DATE(scheduled_departure) AS dep_date,
@@ -9,6 +10,7 @@ FROM flights
 ORDER BY route_no, dep_date, scheduled_departure;
 
 
+-- Порядковый номер рейса внутри маршрута
 SELECT
     route_no,
     scheduled_departure,
@@ -17,5 +19,13 @@ SELECT
         ORDER BY scheduled_departure
     ) AS flight_number_in_route
 FROM flights;
---rgtret
+
+
+-- Средняя длительность полетов между одинаковыми парами аэропортов
+SELECT
+    departure_airport,
+    arrival_airport,
+    duration,
+    AVG(duration) OVER (PARTITION BY departure_airport, arrival_airport) AS flights_avg
+FROM routes;
 
